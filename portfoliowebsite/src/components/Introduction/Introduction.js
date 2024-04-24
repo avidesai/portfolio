ea352b4afc3720f34b97f5af53829337ffd5d9e1
avi <avidesai2@gmail.com>
2024-04-24 03:48:08 -0700

    Updated intro

--- portfoliowebsite/src/components/Introduction/Introduction.js
+++ portfoliowebsite/src/components/Introduction/Introduction.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Introduction.css';
+import profilePic from '../../images/profile.png'; // Add your profile image in the images directory
 
 const Introduction = () => {
   return (
     <section id="about">
+      <div className="profile-pic-container">
+        <img src={profilePic} alt="Avi Desai" className="profile-pic" />
+      </div>
       <h1>Avi Desai</h1>
       <p>
-        Hello, I'm Avi &#128075; 
+        Hello, I'm Avi &#128075;
         I am a passionate software developer with a strong interest in machine learning and intelligent systems. I enjoy working on challenging projects and leveraging my skills in various technologies to solve problems. Let's build something exceptional together!
       </p>
     </section>
