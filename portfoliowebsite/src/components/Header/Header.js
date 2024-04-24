c068ecd8dbb26a53c217f9ea7f478fa3048f7c61
avi <avidesai2@gmail.com>
2024-04-24 03:57:12 -0700

    Made a few changes

--- portfoliowebsite/src/components/Header/Header.js
+++ portfoliowebsite/src/components/Header/Header.js
@@ -9,6 +9,7 @@ const Header = () => {
           <li><a href="#projects">Projects</a></li>
           <li><a href="#skills">Skills</a></li>
           <li><a href="#experience">Experiences</a></li>
+          <li><a href="#education">Education</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
       </nav>
